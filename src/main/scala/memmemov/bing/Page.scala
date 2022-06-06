84f644056c501d246c7d68a5a3013e3ec9b68226
Alexander Novikov <mem-memov@yandex.ru>
2022-06-06 22:59:09 +0300

    pages

--- src/main/scala/memmemov/bing/Page.scala
+++ src/main/scala/memmemov/bing/Page.scala
@@ -2,7 +2,26 @@ package memmemov.bing
 
 import scala.scalanative.libc.stdlib.malloc
 import scala.scalanative.unsafe.UWord
+import scala.scalanative.unsigned.UByte
 
 class Page(
-          private val lines: List[Line]
-          )
+  private val level: Int
+):
+
+  private lazy val level = 0
+
+  private lazy val lines = new Array[Line](256)
+
+  private lazy val parent = new Page()
+
+  private lazy val children = new Array[Option[Page]](256)
+
+  def content(address: List[UByte]): UByte = ???
+
+  def write(address: List[UByte], content: UByte): Unit = ???
+
+  def close(): Unit = ???
+
+object Page:
+
+  def apply(): Page = new Page(1)
